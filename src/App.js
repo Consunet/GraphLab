aa76044b1af09e40f10151feebf7de7f48798fab
rama <rama@hammer.com>
2019-03-31 21:34:52 +1030

    React application added.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <div id="GraphLabDiv" style={{width: 800 + 'px', height: 400 + 'px'}} />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import GraphLab from '@consunet/graph-lab';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+var graph = new GraphLab.graph('GraphLabDiv');
+
+var offset = 0;
+
+graph.insertFunction(function(x, z) {return window.Amplitude * Math.cos(x + offset) * Math.cos(z + offset);}, 'cos(x) \u00d7 cos(z)');
+graph.insertFunction(function(x, z) {return window.Amplitude * Math.exp(x) * z / 100;}, 'exp(x) \u00d7 z');
+
+graph.insertSwitchPanel();
+graph.createParameterBlock();
+graph.createParameter('Amplitude', 0, 1, 0.01, 1);
+
+graph.range_x(Math.PI * -2, Math.PI * 2, Math.PI * 0.125);
+graph.range_z(Math.PI * -2, Math.PI * 2, Math.PI * 0.125);
+graph.range_y(-4, 4);
+
+graph.scaling = 0.4;
+graph.reRoll(-0.5, -0.5);
+
+setInterval(function() {graph.reGraph(); offset += 0.04;}, 100);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
